d15783490f5e1b7d657df95068aedcdbf6321812
Avelino <avelinorun@gmail.com>
2023-06-10 15:18:25 -0300

    webview: start restricting navigation logic on external link
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import photo_manager
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
